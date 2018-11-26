4141eba4d6b13fcfa921a77abe0adb7ef8a41a5b
sarah <sarah@Beccas-MBP-2.localdomain>
2018-11-26 15:30:36 +0000

    Reduce my paper/rock/scissors.rb files to one : elements.rb

--- spec/elements_spec.rb
+++ spec/elements_spec.rb
@@ -1,16 +1,19 @@
 # This is my elements spec file.
 # I will test here :
-#     - the behaviour of the rock
 #     - the behaviour of the paper
+#     - the behaviour of the rock
 #     - the behaviour of the scissors
 
-require 'paper'
-require 'rock'
-require 'scissors'
+require 'elements'
 
-RSpec.describe Paper do
-  it 'ask if paper is an object' do
-    user = Person.new("Sarah", "Kharraz", "s@k.com", 123456789)
+RSpec.describe Elements do
 
-    expect(user.first_name).to eq("Sarah")
+  it 'ask if paper/rock/scissors are an object' do
+    elements = Elements.new
+
+    expect(elements.is_a?Object).to eq(true)
   end
+
+  xit ''
+
+end
