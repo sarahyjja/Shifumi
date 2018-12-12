90e972539fec5f4eaf4abfae50df269aa113c4c7
sarahyjja <sarahkharraz@gmail.com>
2018-12-12 16:28:36 +0000

    Computer vs Computer is OK

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,9 +1,25 @@
 require 'move_randomizer'
 
  class Computer
+   def initialize
+     @name = ""
+     @move = ""
+   end
+
+   def name
+     @name
+   end
+
+   def move
+     @move
+   end
+
+  def create_player_name
+    @name = "Computer"
+  end
 
    def make_move
     move_randomizer = MoveRandomizer.new
-    move_randomizer.move
+    @move = move_randomizer.move
    end
  end
