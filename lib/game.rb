275ed06948006a247eac0e07d43a890fdf3a6016
sarahyjja <sarahkharraz@gmail.com>
2018-12-12 14:33:09 +0000

    Play again

--- lib/game.rb
+++ lib/game.rb
@@ -21,40 +21,13 @@ class Game
     move_player_2 = rules.make_move(player_2)
 
     winner = rules.winning_move(move_player_1, move_player_2)
-    
+
     if winner
       puts "#{winner} you win!"
     else
       puts "Tie!"
     end
+    play
   end
+
 end
-
-  # laurent sans changement _____________
-  # def start
-  #   player_1 = @setup.create_player_1
-  #   player_2 = @setup.create_player_2
-  #
-  #   move_player_1 = player_1.make_move
-  #   move_player_2 = player_2.make_move
-  #
-  #   winner = get_winner(move_player_1, move_player_2)
-  #   puts winner
-  # end
-
-
-# le mien _____________
-#   def play
-#     players = @setup.ask_for_game_mode
-#     player1 = players[0]
-#     player2 = players[1]
-#     @setup.display_player_name(player1.name, player2.name)
-#     player1.ask_for_move(player1.name)
-#     # make_move
-#     convert_input_to_move(choice)
-#     ask_for_move(player2)
-#     make_move
-#     convert_input_to_move(choice)
-#     winning_move(@moves)
-#   end
-# end
